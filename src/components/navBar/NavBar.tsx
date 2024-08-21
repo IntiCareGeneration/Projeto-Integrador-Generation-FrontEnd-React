7e4e84f545ad1e007e44c0b69334b8497b128f17
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-08-21 13:48:20 -0300

    mudança cor navbar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -6,10 +6,10 @@ import logoNavbar from '/src/assets/logo-navbar-novo.png'
 function Navbar() {
     return (
         <>
-            <div className="navbar-container bg-gradient-to-b from-green-100 to to-white text-slate-950">
+            <div className="navbar-container bg-gradient-to-b from-sky-100 to to-white text-slate">
                 <div className="navbar-content">
                 <Link to="/home" className="text-2xl font-bold uppercase flex items-center">
-                <img src={logoNavbar} alt="Logo Inticare" className="h-16 w-auto mr-2" /></Link>
+                <img src={logoNavbar} alt="Logo Inticare" className="h-14 w-auto mr-2" /></Link>
 
                     <div className="navbar-links">
                         <Link to="/sobre" className="navbar-link">Sobre</Link>
@@ -17,8 +17,8 @@ function Navbar() {
                         <Link to="/ajuda" className="navbar-link">Ajuda</Link>
                         <Link to="/parcerias" className="navbar-link">Parcerias</Link>
                         <Link to="/cadastro" className="navbar-link">Cadastro</Link>
-                        <Link to='/categorias' className='hover:underline'>Categorias</Link>
-                        <Link to='/cadastroCategoria' className='hover:underline'>CadastrarCategoria</Link>
+                        <Link to='/categorias' className="navbar-link">Categorias</Link>
+                        <Link to='/cadastroCategoria' className="navbar-link">CadastrarCategoria</Link>
                         <Link to="/login" className="navbar-link">Login</Link>
                         <Link to="/contato" className="navbar-link">Contato</Link>
                         
