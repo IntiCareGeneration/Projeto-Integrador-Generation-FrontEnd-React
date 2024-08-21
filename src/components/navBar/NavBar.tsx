45a8ad8031e1d18e79658492949ade925c142d91
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-08-21 11:36:00 -0300

    atualização index.html e navbar.tsx

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
+import logoNavbar from '/src/assets/logo-navbar-novo.png'
 
 function Navbar() {
     return (
         <>
-            <div className="navbar-container">
+            <div className="navbar-container bg-gradient-to-b from-green-100 to to-white text-slate-950">
                 <div className="navbar-content">
-                    <div className="navbar-brand">IntiCare</div>
+                <Link to="/home" className="text-2xl font-bold uppercase flex items-center">
+                <img src={logoNavbar} alt="Logo Inticare" className="h-16 w-auto mr-2" /></Link>
+
                     <div className="navbar-links">
-                        <Link to="/home" className="navbar-link">Home</Link>
                         <Link to="/sobre" className="navbar-link">Sobre</Link>
                         <Link to="/educacao" className="navbar-link">Educação</Link>
-                        <Link to="/contato" className="navbar-link">Contato</Link>
                         <Link to="/ajuda" className="navbar-link">Ajuda</Link>
                         <Link to="/parcerias" className="navbar-link">Parcerias</Link>
-                        <Link to="/login" className="navbar-link">Login</Link>
                         <Link to="/cadastro" className="navbar-link">Cadastro</Link>
                         <Link to='/categorias' className='hover:underline'>Categorias</Link>
                         <Link to='/cadastroCategoria' className='hover:underline'>CadastrarCategoria</Link>
+                        <Link to="/login" className="navbar-link">Login</Link>
+                        <Link to="/contato" className="navbar-link">Contato</Link>
+                        
                     </div>
                 </div>
             </div>
