fde96a6631c306939489d327b180cc26ea0a53f5
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-08-21 16:32:13 -0300

    estilização home

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -18,7 +18,7 @@ function Navbar() {
 
     return (
         <>
-            <div className="navbar-container bg-gradient-to-b from-sky-100 to to-white text-slate">
+            <div className="navbar-container bg-gradient-to-b from-sky-100 to to-white">
                 <div className="navbar-content">
                 <Link to="/home" className="text-2xl font-bold uppercase flex items-center">
                 <img src={logoNavbar} alt="Logo Inticare" className="h-14 w-auto mr-2" /></Link>
@@ -28,11 +28,12 @@ function Navbar() {
                         <Link to="/educacao" className="navbar-link">Educação</Link>
                         <Link to="/ajuda" className="navbar-link">Ajuda</Link>
                         <Link to="/parcerias" className="navbar-link">Parcerias</Link>
-                        <Link to="/cadastro" className="navbar-link">Cadastro</Link>
                         <Link to='/categorias' className="navbar-link">Categorias</Link>
-                        <Link to='/cadastroCategoria' className="navbar-link">CadastrarCategoria</Link>
+                        <Link to='/cadastroCategoria' className="navbar-link">Cadastrar Categoria</Link>
                         <Link to="/login" className="navbar-link">Login</Link>
                         <Link to='/perfil' className="navbar-link">Perfil</Link>
+                        <Link to="/cadastro" className="navbar-link">Cadastro</Link>
+                        <Link to="/login" className="navbar-link">Login</Link>
                         <Link to='' onClick={logout} className="navbar-link">Sair</Link>
                         
                     </div>
