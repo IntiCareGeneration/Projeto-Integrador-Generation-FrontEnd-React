89d8f5ef1ff233607a83c0df17e466408673d0b9
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-08-22 17:12:08 -0300

    ajustes no perfil

--- src/paginas/perfil/Perfil.tsx
+++ src/paginas/perfil/Perfil.tsx
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-import loginLogo from '../../assets/login.jpg'
 import { toastAlerta } from '../../util/toastAlerta'
+import fotoPerfil from '../../assets/perfil.png'
+import cuidadoImage from "../../assets/cuidado.jpg"
+import './Perfil.css';
+
 function Perfil() {
   let navigate = useNavigate()
 
@@ -17,11 +20,11 @@ function Perfil() {
     
   return (
     <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
-      <img className='w-full h-72 object-cover border-b-8 border-white' src={loginLogo} alt="Capa do Perfil" />
-      <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
-      <div className="relative mt-[-6rem] h-72 flex flex-col bg-sky-500 text-white text-2xl items-center justify-center">
-        <p>Nome: {usuario.nome} </p>
-        <p>Email: {usuario.usuario}</p>
+      <img className='w-full h-72 object-cover border-b-8 border-white' src={cuidadoImage} alt="Capa do Perfil" />
+      <img src={fotoPerfil} alt='Foto de perfil' className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
+      <div className="relative mt-[-6rem] h-72 flex flex-col bg-sky-100 items-center justify-center">
+        <p className='text'>Nome: {usuario.nome} </p>
+        <p className='text'>Email: {usuario.usuario}</p>
       </div>
     </div>
   )
